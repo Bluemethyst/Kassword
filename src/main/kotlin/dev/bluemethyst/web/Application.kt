f9804643a1729b68b3755f1164b7c6dbc2ce2747
Bluemethyst <bluemethyst@gmail.com>
2024-05-20 10:36:32 +1200

    Begun port to Kotlin from Python

---
+++ src/main/kotlin/dev/bluemethyst/web/Application.kt
@@ -0,0 +1,11 @@
+package dev.bluemethyst.web
+
+import dev.bluemethyst.web.plugins.configureRouting
+import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::configureRouting).start(wait = true)
+}
